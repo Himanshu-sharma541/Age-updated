67217bc54e063f5b9721043add50407a2b9c6ae4
Himanshu sharma <155475886+Himanshu-sharma541@users.noreply.github.com>
2025-06-19 17:58:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var age = 25
+
+function displayage(){
+    console.log("crunt age is", age);
+}
+
+function changeage(){
+    age =30;
+    console.log("age is changed to",age);
+}
+displayage();
+changeage();
+displayage();
